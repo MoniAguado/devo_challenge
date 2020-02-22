c9e1f206103a70620194fd8a4cecd16ec9d13c92
Monica <maguado85@gmail.com>
2020-02-22 14:17:29 +0100

    Chore: starting tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() })
+
+// describe('App', () => {
+//   it('llama a handleTodo cuando introduce un valor en input', () => {
+//     const handleFilter = jest.fn();
+//     const wrapper = shallow(<App handleFilter={handleFilter}/>)
+
+//     wrapper
+//       .find('input')
+//       .simulate('change', { target : { value: 'Madrid'}})
+
+//     expect(true).toBeTruthy([['Madrid']])
+  
+    
+//   })
+// })
+
+
+describe('App', () => {
+  it('App should render correctly', () => {
+    const component = shallow(<App debug/>);
+
+    component
+      .find('input')
+      .simulate('inputText', { value: 'Madrid'})
+  
+    expect(component).toMatchSnapshot();
+    component.unmount();
+  });
+  // describe('Home', () => {
+  //   it('home component should render correctly', () => {
+  //     const home = shallow(<Home />);
+    
+  //     expect(home).toMatchSnapshot();
+  //   });
+  // });
+  // describe('Detail', () => {
+  //   it('detail component should render correctly', () => {
+  //     const detail = shallow(<Detail />);
+    
+  //     expect(detail).toMatchSnapshot();
+  //   });
+  // });
+
+  // describe('<Home /> rendering', () => {
+  //   it('should render one <ul>', () => {
+  //       let wrapper = shallow(<Home />);
+  //       expect(wrapper.children('ul')).toHaveLength(1);
+  //   });
+// });
 });
+
+
+
