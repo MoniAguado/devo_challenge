a70e1b6300b5f863acabc3a5b994ab1c811c0819
Monica <maguado85@gmail.com>
2020-02-23 21:41:32 +0100

    Chore: trying new methods to testingcomponents and functionalities

--- src/App.test.js
+++ src/App.test.js
@@ -15,14 +15,21 @@ describe('App', () => {
     expect(component).toMatchSnapshot();
     component.unmount();
   });
-  describe('Home', () => {
-    it('home component should render correctly', () => {
-      const home = shallow(<Home />);
+});
+
+  //  TRYING TESTING: I try to test the components and some functionalitys the project have with everal methods:
+
+  // 1 methos:
+
+
+  // describe('Home', () => {
+  //   it('home component should render correctly', () => {
+  //     const home = shallow(<Home />);
     
-      expect(home).toMatchSnapshot();
-      home.unmount();
-    });
-  });
+  //     expect(home).toMatchSnapshot();
+  //     home.unmount();
+  //   });
+  // });
   // describe('Detail', () => {
   //   it('detail component should render correctly', () => {
   //     const detail = shallow(<Detail />);
@@ -32,7 +39,52 @@ describe('App', () => {
   // });
 
  
-});
+
+// 2 method:
+
+// describe('<Home />', () => {
+//   it('adds checked attribute to input', () => {
+//     const handleFilter =jest.fn();
+//     const handleSortMax = jest.fn();
+//     const handleSortMin= jest.fn();
+//     const index = 6;
+
+
+//     const wrapper = shallow(
+//     <Home
+//       handleFilter={handleFilter}
+//       handleSortMax={handleSortMax}
+//       handleSortMin={handleSortMin}
+//     />
+//     );
+
+//     wrapper
+//       .find('input')
+//       .at(0)
+//       .simulate('click')
+
+//       expect(completeTodo.mock.calls).toEqual([[6]])
+//       expect(removeTodo.mock.calls).toEqual([])
+//   });
+
+
+// 3 method:
+
+  // it('adds disabled attribute to input', () => {
+  //   const wrapper = shallow(<Home disabled />);
+  //   const input = wrapper.find('input');
+
+  //   expect(input.props().disabled).toBe(true);
+  // });
+
+  // it('calls the onChange function', () => {
+  //   const onChange = expect.createSpy();
+  //   const wrapper = mount(<Home onChange={this.handleFilter} />);
+  //   const input = wrapper.find('input');
+
+  //   input.simulate('change');
+  //   expect(onChange).toHaveBeenCalled();
+  // });
 
 
 
